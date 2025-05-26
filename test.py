74e6891f50774c9a21fcaae6567f1ad9ff6b4548
Mohsin Ullah Shah <mohsinullahshah@Mohsins-MacBook-Pro.local>
2025-05-26 08:20:46 +0500

    Chapter 11

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-filename = "learning_python.txt"
+def city_country(city, country,population=5000000):
+    return f"{city}, {country}, {population}"
 
-# Read the entire file
-with open(filename) as file:
-    contents = file.read()
-print("=== Read entire file ===")
-print(f'Content is {contents}')
 
-# Loop over the file object
-print("\n=== Loop over file object ===")
-with open(filename) as file_object:
-    for line in file_object:
-        print(line.strip())
